8c7179b0b73d3bc238709af9d6879b043fcd2b65
2MuchC0ff33 <adriangallo87@gmail.com>
2023-02-16 15:21:36 +0800

    added execution policy and included ms update

--- windows_update/check_for_and_install_windows_updates.ps1
+++ windows_update/check_for_and_install_windows_updates.ps1
@@ -1,5 +1,8 @@
 # Check for and install Windows updates
+Set-ExecutionPolicy Unrestricted
+Install-Module -Name PSWindowsUpdate
+Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -AddServiceFlag 7
 $updates = Get-WindowsUpdate
 if ($updates) {
-    Install-WindowsUpdate -Updates $updates -AcceptAll -AutoReboot
+    Install-WindowsUpdate -MicrosoftUpdate $updates -AcceptAll -AutoReboot
 }
